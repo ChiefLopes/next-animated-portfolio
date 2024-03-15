1646b1f233c437b32167767696ed4b82194a5124
ChiefLopes <chieflopesekede@gmail.com>
2024-03-15 19:16:15 +0100

    added the toggleMenu and setToggleMenu state to toglle the menu bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
 import { NavLinks } from "../../utils/constants";
 
-const navbar = () => {
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   console.log(NavLinks);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
@@ -22,23 +26,32 @@ const navbar = () => {
       {/* RESPONSIVE MENU */}
       <div>
         {/* MENU BUTTON */}
-        <button className="w-10 h-7 flex flex-col justify-between cursor-pointer ">
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+        <button
+          className="w-10 h-7 flex flex-col justify-between cursor-pointer z-50 relative "
+          onClick={() => setToggleMenu(!toggleMenu)}
+        >
+          <div className="w-10 h-1 rounded bg-white"></div>
+          <div className="w-10 h-1 rounded bg-white"></div>
+          <div className="w-10 h-1 rounded bg-white"></div>
         </button>
 
         {/* MENU LIST */}
-        <div className="absolute top-0  left-0 w-screen h-screen text-white">
-          {NavLinks.map((link) => (
-            <Link href={link.url} key={link.title}>
-              {link.title}
-            </Link>
-          ))}
-        </div>
+        {toggleMenu && (
+          <div className="absolute top-0  left-0 w-screen h-screen text-white bg-black flex flex-col items-center justify-center gap-8 text-4xl">
+            {NavLinks.map((link) => (
+              <Link
+                href={link.url}
+                key={link.title}
+                className="hover:text-blue-400 transition-all ease-in-out duration-300 "
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default navbar;
+export default Navbar;
