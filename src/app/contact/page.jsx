5ccd7be858a8bafb324c60327cc0855bd6b4da12
ChiefLopes <chieflopesekede@gmail.com>
2024-03-17 23:31:04 +0100

    added framer motion effects to the pages

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,16 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
 
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
   return (
-      <div>ContactPage
-          
-          <Link href="/about">
-              Go to about
-          </Link>
-    </div>
-  )
-}
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      Contact Page
+    </motion.div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
