8fae95102fb740f060e88531b6db19f2530bba54
ChiefLopes <chieflopesekede@gmail.com>
2024-03-19 02:28:29 +0100

    working on hire me

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -26,8 +26,8 @@ const PortfolioPage = () => {
           My Works
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center ">
-          <motion.div style={{x}} className="flex">
-            <div className=""/>
+          <motion.div style={{ x }} className="flex">
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
             {items.map((item) => (
               <div
                 key={item.id}
@@ -36,7 +36,7 @@ const PortfolioPage = () => {
                 <div className="flex flex-col gap-8 text-white ">
                   <h1>{item.title}</h1>
                   <div className="relative">
-                    <Image src={item.img} alt="image" fill/>
+                    <Image src={item.img} alt="image" fill />
                   </div>
                   <p>{item.desc}</p>
                   <Link href={item.link}>
@@ -46,6 +46,25 @@ const PortfolioPage = () => {
               </div>
             ))}
           </motion.div>
+        </div>
+      </div>
+      <div className="bg-red-500 w-screen h-screen flex flex-col items-center justify-center text-center gap-16 ">
+        <h1>Do you have a project </h1>
+        <div className="relative ">
+          <svg viewBox="0 0 300 300">
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0"
+              />
+            </defs>
+          </svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full"
+          >
+            Hire Me
+          </Link>
         </div>
       </div>
     </motion.div>
